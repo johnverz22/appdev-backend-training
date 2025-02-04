c30af809460fdae05b965ecf98cffc86a9ae623a
johnverz22 <johnny.verzola@lorma.edu>
2025-02-04 12:10:43 +0800

    Updated apis with comments

--- routes/api.php
+++ routes/api.php
@@ -9,9 +9,14 @@ use App\Models\Post;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
+//Show all posts
 Route::get("/post", [PostController::class, "index"]);
+//Show specific post
 Route::get("/post/{post}", [PostController::class, "show"]);
+//Store or create new post
 Route::post("/post", [PostController::class,"store"]);
+//Delete a post
 Route::delete("/post/{post}", [PostController::class, "destroy"]);
+//Update a post
 Route::put("/post/{post}", [PostController::class, "update"]);
 
